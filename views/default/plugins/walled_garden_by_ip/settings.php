103d0af234244100706c3f6841af67611475f95c
Jerome Bakker <jeabakker@coldtrick.com>
2012-04-10 21:58:47 +0200

    changes for Elgg 1.8

--- views/default/plugins/walled_garden_by_ip/settings.php
+++ views/default/plugins/walled_garden_by_ip/settings.php
@@ -4,6 +4,6 @@
 	
 	echo "<div>";
 	echo "<label>" . elgg_echo("walled_garden_by_ip:settings:allowed_ip") . "</label>";
-	echo elgg_view("input/plaintext", array("internalname" => "params[allowed_ip]", "value" => $plugin->allowed_ip));
+	echo elgg_view("input/plaintext", array("name" => "params[allowed_ip]", "value" => $plugin->allowed_ip));
 	echo "<div class='elgg-subtext'>" . elgg_echo("walled_garden_by_ip:settings:allowed_ip:description") . "</div>";
 	echo "</div>";
